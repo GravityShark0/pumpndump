a58d14cac4e3e7986ef25362d31c3fcef8128af0
gravitys <alternativegravitational@protonmail.com>
2024-01-24 19:39:51 +0800

    yeahhhhh

--- home/.scripts/monitor/auto.sh
+++ home/.scripts/monitor/auto.sh
@@ -2,10 +2,14 @@
 
 if [ $DISPLAY ]; then
     hdmi_status=$(cat /sys/class/drm/card1-HDMI-A-2/status)
+    if [ ! $hdmi_status ]; then
+        hdmi_status=$(cat /sys/class/drm/card0-HDMI-A-2/status)
+    fi
     xrandr --setprovideroutputsource modesetting NVIDIA-0
     if [ "$hdmi_status" = "connected" ]; then
         xrandr --output HDMI-0 --primary --mode 1440x900 --pos 0x0 --rotate normal --output eDP-1-1 --off
     else
-        xrandr --output HDMI-0 --primary --mode 1440x900 --pos 0x0 --rotate normal --output eDP-1-1 --mode 1600x900 --pos 1440x0 --rotate normal
+        xrandr --output eDP-1-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-0 --off
+        # xrandr --output HDMI-0 --primary --mode 1440x900 --pos 0x0 --rotate normal --output eDP-1-1 --mode 1600x900 --pos 1440x0 --rotate normal
     fi
 fi
