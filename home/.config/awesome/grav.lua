41a507b90a2460c8d3bd18613edfeb4a70daf8f1
gravitys <alternativegravitational@protonmail.com>
2024-02-10 23:28:39 +0800

    fuck me bro

--- home/.config/awesome/grav.lua
+++ home/.config/awesome/grav.lua
@@ -59,6 +59,7 @@ theme.taglist_fg_empty = theme.bg_normal
 -- notification_[bg|fg]
 -- notification_[width|height|margin]
 -- notification_[border_color|border_width|shape|opacity]
+theme.notification_icon_size = dpi(100)
 
 -- Variables set for theming the menu:
 -- menu_[bg|fg]_[normal|focus]
