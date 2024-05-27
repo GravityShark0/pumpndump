076b577a5e0c7f315df8c764f0e5ebf36b77d1f1
Gravity <alternativegravitational@protonmail.com>
2024-05-27 12:35:37 +0800

    yeert

---
+++ home/.config/sesh/scripts/coollittlewebsite.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+tmux split-window -v -l 5
+tmux send-keys "z scripts/" Enter "./tailwindcss.sh --watch" Enter
+tmux select-pane -t :.+
+tmux send-keys "air" Enter
+tmux new-window
+tmux send-keys "vis" Enter
