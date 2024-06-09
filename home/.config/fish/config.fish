e5d68a85b02930a9af63c61128f547e2c86c1d22
Gravity <alternativegravitational@protonmail.com>
2024-06-10 00:16:04 +0800

    yoooo

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,8 +1,7 @@
-set_color red
 if not source $__fish_user_data_dir/plugins/plug.fish/conf.d/plugin_load.fish
     set_color green; echo -e "To install plug.fish do: \ncurl -L https://github.com/kidonng/plug.fish/raw/v3/conf.d/plugin_load.fish | plugins=https://github.com/kidonng/plug.fish source"
+    set_color normal
 end
-set_color normal
 
 if status is-interactive
     fzf_key_bindings        
