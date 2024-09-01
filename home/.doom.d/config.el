cef32a065bed6349c40c2fe8e45fe0799ee05833
Gravity <alternativegravitational@protonmail.com>
2024-09-01 09:15:36 +0800

    asser

--- home/.doom.d/config.el
+++ home/.doom.d/config.el
@@ -197,20 +197,23 @@
 
 ;;; Custom commands
 (defun calculate-pomodoro-time (input)
-  "Damn this is a cmmoent"
+  "Calculate how long it would take for how many pomodoros"
   (interactive "nHow many pomodoros: ")
-  (let ((mins 0))
+  (let ((pomodoro 25)
+        (shortbreak 5)
+        (longbreak 15)
+        (longbreakinterval 4)
+        (mins 0))
     (dotimes (i input)
-      (if (= (% (+ i 1) 4) 0)
-          (setq mins (+ mins 40))
-        (setq mins (+ mins 30))))
-
-    (let* ((current-time (current-time))        ; Get the current time
-           (time-delta (* mins 60))          ; Convert minutes to seconds
+      (if (= (% (+ i 1) longbreakinterval) 0)
+          (setq mins (+ mins pomodoro longbreak)))
+      (setq mins (+ mins pomodoro shortbreak)))
+    (let* ((current-time (current-time))
+           (time-delta (* mins 60))
            (future-time (time-add current-time (seconds-to-time time-delta)))
            (hours (/ mins 60))
            (remaining-minutes (% mins 60))
            (longing (format "%d:%d" hours remaining-minutes)))
-      (print (format "%i Pomodoro(s) is %s long and it would end at %s" input longing (format-time-string "%l:%M %p" future-time))))))
-
-;; (message "You entered: %i" input))
+      (print (format "%s long, ends %s" longing (format-time-string "%I:%M%p" future-time))))))
+;; How many pomodoros: 9
+;; "5:50 long, ends 3:03PM"
